4830d6428ef5f1fa2ee9d0298d199b291f6253fc
Aj1st-cons <admin@aj1st.com>
2025-02-07 06:36:33 +0400

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,6 @@
+const categoryUrls = {
+            "vegetables": "https://order-1ib.pages.dev/Vegetables.html",
+            "fruits": "https://order-1ib.pages.dev/Fruits.html",
+            "meat": "https://order-1ib.pages.dev/Meat.html",
+            "seafood": "https://order-1ib.pages.dev/Seafood.html"
+        };
