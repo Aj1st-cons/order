a373efce8911e0fb8c3f9c1073aa9fce1fe279cf
Aj1st-cons <admin@aj1st.com>
2025-02-07 16:44:42 +0400

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,6 +1,7 @@
 const categoryUrls = {
             "vegetables": "https://order-1ib.pages.dev/Vegetables.html",
             "fruits": "https://order-1ib.pages.dev/Fruits.html",
+            "oils": "https://order-1ib.pages.dev/Oils.html",
             "meat": "https://order-1ib.pages.dev/Meat.html",
             "seafood": "https://order-1ib.pages.dev/Seafood.html"
         };
