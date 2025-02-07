beb1714ed35092db150e30980b35d40053a9bb6c
Aj1st-cons <admin@aj1st.com>
2025-02-07 05:26:04 +0400

    Create coordinates.js

---
+++ coordinates.js
@@ -0,0 +1,54 @@
+let locations = {
+    "Dubai": { 
+"City A1": { lat: 25.123, lng: 55.123 }, 
+"City A2": { lat: 25.223, lng: 55.223 }, 
+"City A3": { lat: 25.323, lng: 55.323 },
+"City A4": { lat: 25.123, lng: 55.123 }, 
+"City A5": { lat: 25.223, lng: 55.223 }, 
+"City A6": { lat: 25.323, lng: 55.323 },
+"City A7": { lat: 25.123, lng: 55.123 }, 
+"City A8": { lat: 25.223, lng: 55.223 }, 
+"City A9": { lat: 25.323, lng: 55.323 },
+"City A10": { lat: 25.123, lng: 55.123 }, 
+"City A11": { lat: 25.223, lng: 55.223 }, 
+"City A12": { lat: 25.323, lng: 55.323 },
+"City A13": { lat: 25.123, lng: 55.123 }, 
+"City A14": { lat: 25.223, lng: 55.223 }, 
+"City A15": { lat: 25.323, lng: 55.323 } 
+    },
+    "Abu Dhabi": { 
+"City C1": { lat: 27.123, lng: 57.123 }, 
+"City C2": { lat: 27.223, lng: 57.223 }, 
+"City C3": { lat: 27.323, lng: 57.323 } 
+    },
+    "Ajman": { 
+"City C1": { lat: 27.123, lng: 57.123 }, 
+"City C2": { lat: 27.223, lng: 57.223 }, 
+"City C3": { lat: 27.323, lng: 57.323 } 
+    },
+    "UAQ": { 
+"City C1": { lat: 27.123, lng: 57.123 }, 
+"City C2": { lat: 27.223, lng: 57.223 }, 
+"City C3": { lat: 27.323, lng: 57.323 }  
+    },
+    "RAK": { 
+"City C1": { lat: 27.123, lng: 57.123 }, 
+"City C2": { lat: 27.223, lng: 57.223 }, 
+"City C3": { lat: 27.323, lng: 57.323 }  
+    },
+    "Fujairah": { 
+"City C1": { lat: 27.123, lng: 57.123 }, 
+"City C2": { lat: 27.223, lng: 57.223 }, 
+"City C3": { lat: 27.323, lng: 57.323 }  
+    }
+};
+
+let vendors = {
+"A": { lat: 25.125, lng: 55.126 },
+"AJ": { lat: 24.396973, lng: 54.588957 },
+"AD": { lat: 24.391549, lng: 54.578581 },
+"ND": { lat: 26.125, lng: 56.126 },
+"GF": { lat: 25.225, lng: 55.226 },
+"C": { lat: 26.125, lng: 56.126 },        
+"D": { lat: 27.125, lng: 57.126 }
+};
