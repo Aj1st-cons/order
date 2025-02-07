298946b641c1757723e1b5c1cc13e61e47fec095
Aj1st-cons <admin@aj1st.com>
2025-02-07 06:34:21 +0400

    Update page.js

--- page.js
+++ page.js
@@ -13,12 +13,8 @@ async function loadCategories() {
   loadCategories();
   
     function showItems(category) {
-        const categoryUrls = {
-            "vegetables": "https://order-1ib.pages.dev/Vegetables.html",
-            "fruits": "https://order-1ib.pages.dev/Fruits.html",
-            "meat": "https://order-1ib.pages.dev/Meat.html",
-            "seafood": "https://order-1ib.pages.dev/Seafood.html"
-        };
+
+//××××××××××××××××××××××××××××××××××××        
 
         fetch(categoryUrls[category])
             .then(response => {
