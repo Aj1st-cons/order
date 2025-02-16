b46713a7a3d466db89d4baeaf0603bb516277188
Aj1st-cons <admin@aj1st.com>
2025-02-16 10:23:53 +0400

    Create affiliates-commission.js

---
+++ affiliates-commission.js
@@ -0,0 +1,41 @@
+const affiliatesData = [
+  {
+    "REF-CODE": "",
+    "vendor": "",
+    "address": "",
+    "coordinates": "",
+    "contact No.": "",
+    "total sales": "",
+    "paid for sales": "",
+    "balance to be paid": "",
+    "Total affiliate's commission": "",
+    "payable affiliate's commission": "",
+    "balance affiliate's commission": ""
+  },
+  {
+    "REF-CODE": "DFR856",
+    "vendor": "vendor 1",
+    "address": "Shop no 12, B Block, Al Nadah, Sharjah",
+    "coordinates": "24.39360, 54.58439",
+    "contact No.": 568245367,
+    "total sales": 100,
+    "paid for sales": 70,
+    "balance to be paid": 30,
+    "Total affiliate's commission": 30,
+    "payable affiliate's commission": 21,
+    "balance affiliate's commission": 9
+  },
+  {
+    "REF-CODE": "DFR858",
+    "vendor": "vendor 2",
+    "address": "Shop no 12, B Block, Al Nadah, Sharjah",
+    "coordinates": "24.39360, 54.58439",
+    "contact No.": 568245367,
+    "total sales": 241,
+    "paid for sales": 141,
+    "balance to be paid": 100,
+    "Total affiliate's commission": 72.3,
+    "payable affiliate's commission": 42.3,
+    "balance affiliate's commission": 30
+  }
+];
