4a3ead455ed69e5837d2919c1575f8e1b999cf6c
Aj1st-cons <admin@aj1st.com>
2025-02-20 22:07:02 +0400

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -51,7 +51,7 @@ let locations = {
 let vendors = {
     "A": { lat: 25.125, lng: 55.126, categories: ["Grocery", "Laundry"] },
     "AJ": { lat: 24.396973, lng: 54.588957, categories: ["Cafeteria"] },
-    "CC": { lat: 24.391549, lng: 54.578581, categories: ["Salon", "Laundry"] },
+    "CC": { lat: 25.124, lng: 55.124, categories: ["Salon", "Grocery"] },
     "ColombianoCofeeHouse": { lat: 24.391548, lng: 54.578581, categories: ["Cafeteria"] },    
     "AD": { lat: 24.3969, lng: 54.5889, categories: ["Grocery"] }
 };
