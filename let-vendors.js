39691befe893c0793d5d48245dea16a7609d3650
Aj1st-cons <admin@aj1st.com>
2025-02-21 12:43:12 +0400

    Create let-vendors.js

---
+++ let-vendors.js
@@ -0,0 +1,7 @@
+let vendors = {
+    "A": { lat: 25.125, lng: 55.126, categories: ["Grocery", "Laundry"] },
+    "AJ": { lat: 24.396973, lng: 54.588957, categories: ["Cafeteria"] },
+    "CC": { lat: 25.124, lng: 55.124, categories: ["Salon", "Grocery"] },
+    "ColombianoCofeeHouse": { lat: 24.391548, lng: 54.578581, categories: ["Cafeteria"] },    
+    "AD": { lat: 24.3969, lng: 54.5889, categories: ["Grocery"] }
+};
