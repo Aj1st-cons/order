1d64842caf0a9a68a9b718b67c47537af7775e0b
Aj1st-cons <admin@aj1st.com>
2025-02-24 12:32:29 +0400

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -25,6 +25,7 @@ let locations = {
 "City C1": { lat: 27.123, lng: 57.123 }, 
 "City C2": { lat: 27.223, lng: 57.223 }, 
 "China Camp": { lat: 24.255, lng: 54.627 } 
+    },
     "Ajman": { 
 "City C1": { lat: 27.123, lng: 57.123 }, 
 "City C2": { lat: 27.223, lng: 57.223 }, 
