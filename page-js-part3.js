bcabd7bbb1cdd5be70f4c5037ab0db3cbdfb59ee
Aj1st-cons <admin@aj1st.com>
2025-02-24 23:52:29 +0400

    Update page-js-part3.js

--- page-js-part3.js
+++ page-js-part3.js
@@ -368,7 +368,7 @@ function showLocationServicesPopup() {
 
     // Error message with bold and red color
     let messageText = document.createElement("p");
-    messageText.innerHTML = "<strong>Unable to get your location</strong><br><span style='font-size:8px;'>Please check your location services in device settings.</span>";
+    messageText.innerHTML = "<strong>Unable to get your location</strong><br><span style='font-size:12px;'>Please check your location services in device settings.</span>";
     
     let okButton = document.createElement("button");
     okButton.textContent = "OK";
