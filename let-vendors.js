74e21de01fdf322f8aa92a05f55ece77309e8a7b
Aj1st-cons <admin@aj1st.com>
2025-03-17 07:34:34 +0400

    Update let-vendors.js

--- let-vendors.js
+++ let-vendors.js
@@ -1,6 +1,6 @@
 let vendors = {
 "aj": { lat: 24.385, lng: 54.584, categories: ["Grocery", "Laundry"] },
-"gh": { lat: 24.255, lng: 54.627, categories: ["Cafeteria"] },
+"gh": { lat: 25.255, lng: 54.627, categories: ["Cafeteria"] },
 "jithu": { lat: 24.375, lng: 54.586, categories: ["Salon", "Grocery"] },
 "b": { lat: 24.410, lng: 54.582, categories: ["Cafeteria", "Grocery"] },    
 "c": { lat: 24.373, lng: 54.580, categories: ["Salon"] },
