32d80f687fe38408064a56d3a341072c399e99c2
Aj1st-cons <admin@aj1st.com>
2025-04-06 13:42:16 +0400

    Update new.js

--- new.js
+++ new.js
@@ -100,10 +100,10 @@ function saveRename() {
 function startLoading() {
     document.getElementById('loadingOverlay').style.display = 'block';
     let countdown = 30;
-    document.getElementById('countdown').textContent = `Receiving location ${countdown} s`;
+    document.getElementById('countdown').textContent = `Receiving location ${countdown}s`;
     countdownInterval = setInterval(() => {
         countdown--;
-        document.getElementById('countdown').textContent = `Receiving location ${countdown} s`;
+        document.getElementById('countdown').textContent = `Receiving location ${countdown}s`;
         if (countdown <= 0) {
             stopLoading();
             showErrorPopup();
