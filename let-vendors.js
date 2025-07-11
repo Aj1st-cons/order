8f4ad5e1d10088b5b02dfd504bba58e0e68357af
Aj1st-cons <admin@aj1st.com>
2025-07-11 18:12:08 +0400

    Update let-vendors.js

--- let-vendors.js
+++ let-vendors.js
@@ -2,11 +2,12 @@ let vendors = {
   "Occations": { lat: 25.3006599, lng: 55.3750735, categories: ["Cakes"] },
   "Rapid": { lat: 25.3018266, lng: 55.378627, categories: ["Salon"] },  
   "AlRas": { lat: 25.3006599, lng: 55.3750735, categories: ["Salon"] },
+  "nbys": { lat: 25.301058, lng: 55.3759718, categories: ["Grocery", "Cafeteria", "Restaurant", "Electronics", "Cakes", "Fashion", "Shoes"] },
   "nbys-Grocery": { lat: 25.301058, lng: 55.3759718, categories: ["Grocery"] },
   "nbys-Cafeteria": { lat: 25.301058, lng: 55.3759718, categories: ["Cafeteria"] },
   "nbys-Restaurant": { lat: 25.301058, lng: 55.3759718, categories: ["Restaurant"] },
   "nbys-Electronics": { lat: 25.301058, lng: 55.3759718, categories: ["Electronics"] },
   "nbys-Fashion": { lat: 25.301058, lng: 55.3759718, categories: ["Fashion"] },
   "nbys-Shoes": { lat: 25.301058, lng: 55.3759718, categories: ["Shoes"] },
-  "nbysCakes": { lat: 25.301058, lng: 55.3759718, categories: ["Cakes"] }
+  "nbys-Cakes": { lat: 25.301058, lng: 55.3759718, categories: ["Cakes"] }
   };
