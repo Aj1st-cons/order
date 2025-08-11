d45a7ab4fc7749c5e91072be71064c90979f6358
Aj1st-cons <admin@aj1st.com>
2025-08-11 11:58:51 +0400

    Update let-vendors.js

--- let-vendors.js
+++ let-vendors.js
@@ -1,5 +1,5 @@
 let vendors = {
-  "DawarAlDhaid": { lat: 25.300490, lng: 55.376445, categories: ["Grocery"] },
+  "DawarAlDhaid": { lat: 25.300490, lng: 55.376445, categories: ["Cosmetics"] },
   "BestCafe": { lat: 25.300452, lng: 55.376473, categories: ["Cafeteria"] },
   "Occations": { lat: 25.3006599, lng: 55.3750735, categories: ["Cakes"] },
   "Rapid": { lat: 25.3018266, lng: 55.378627, categories: ["Salon"] },  
