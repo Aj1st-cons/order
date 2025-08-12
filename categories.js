a92395c96638b8b5132339df20acfb7f9910d434
Aj1st-cons <admin@aj1st.com>
2025-08-12 16:48:07 +0400

    Update categories.js

--- categories.js
+++ categories.js
@@ -1,5 +1,5 @@
 const categoryUrls = {
-            "Starters": "https://nearbysx.pages.dev/Starters.html",
+            "starters": "https://nearbysx.pages.dev/Starters.html",
             "vegetables": "https://order-1ib.pages.dev/Vegetables.html",
             "fruits": "https://order-1ib.pages.dev/Fruits.html",
             "oils": "https://order-1ib.pages.dev/Oils.html",
